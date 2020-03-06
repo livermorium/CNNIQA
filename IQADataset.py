a6effe12cb06cba8318f4c1c6efd8e7a47fa388f
chazzhuang <chazzhuang@tencent.com>
2020-03-06 10:19:19 +0800

    + comment + format

--- IQADataset.py
+++ IQADataset.py
@@ -1,6 +1,6 @@
 # Implemented by Dingquan Li
 # Email: dingquanli@pku.edu.cn
-# Date: 2018/4/19
+# modify: zhisays@outlook.com
 
 import os
 import torch
@@ -13,13 +13,16 @@ import h5py
 
 
 def default_loader(path):
-    return Image.open(path).convert('L') #
+    return Image.open(path).convert("L")  #
 
 
 def LocalNormalization(patch, P=3, Q=3, C=1):
-    kernel = np.ones((P, Q)) / (P * Q)
-    patch_mean = convolve2d(patch, kernel, boundary='symm', mode='same')
-    patch_sm = convolve2d(np.square(patch), kernel, boundary='symm', mode='same')
+    kernel = np.ones((P, Q)) / (P * Q)  # conv
+    # convolve2d -> full 10+2*4, same 10+1*4, valid 10+0*4; symm symmetrical, fill padding with fillvalue
+    patch_mean = convolve2d(patch, kernel, boundary="symm", mode="same")
+    # squre mean
+    patch_sm = convolve2d(np.square(patch), kernel, boundary="symm", mode="same")
+    # TODO such a std? why
     patch_std = np.sqrt(np.maximum(patch_sm - np.square(patch_mean), 0)) + C
     patch_ln = torch.from_numpy((patch - patch_mean) / patch_std).float().unsqueeze(0)
     return patch_ln
@@ -28,53 +31,58 @@ def LocalNormalization(patch, P=3, Q=3, C=1):
 def NonOverlappingCropPatches(im, patch_size=32, stride=32):
     w, h = im.size
     patches = ()
+    # non-overlapping
     for i in range(0, h - stride, stride):
         for j in range(0, w - stride, stride):
-            patch = to_tensor(im.crop((j, i, j + patch_size, i + patch_size)))
+            patch = to_tensor(
+                im.crop((j, i, j + patch_size, i + patch_size))
+            )  # 1,32,32
             patch = LocalNormalization(patch[0].numpy())
             patches = patches + (patch,)
     return patches
 
 
 class IQADataset(Dataset):
-    def __init__(self, conf, exp_id=0, status='train', loader=default_loader):
+    def __init__(self, conf, exp_id=0, status="train", loader=default_loader):
         self.loader = loader
-        im_dir = conf['im_dir']
-        self.patch_size = conf['patch_size']
-        self.stride = conf['stride']
-        datainfo = conf['datainfo']
+        im_dir = conf["im_dir"]
+        self.patch_size = conf["patch_size"]
+        self.stride = conf["stride"]
+        datainfo = conf["datainfo"]
 
-        Info = h5py.File(datainfo, 'r')
-        index = Info['index'][:, int(exp_id) % 1000]
-        ref_ids = Info['ref_ids'][0, :]
-        test_ratio = conf['test_ratio']
-        train_ratio = conf['train_ratio']
-        trainindex = index[:int(train_ratio * len(index))]
-        testindex = index[int((1-test_ratio) * len(index)):]
-        train_index, val_index, test_index = [],[],[]
+        Info = h5py.File(datainfo, "r")
+        index = Info["index"][:, int(exp_id) % 1000]
+        ref_ids = Info["ref_ids"][0, :]
+        test_ratio = conf["test_ratio"]
+        train_ratio = conf["train_ratio"]
+        trainindex = index[: int(train_ratio * len(index))]
+        testindex = index[int((1 - test_ratio) * len(index)) :]
+        train_index, val_index, test_index = [], [], []
         for i in range(len(ref_ids)):
-            train_index.append(i) if (ref_ids[i] in trainindex) else \
-                test_index.append(i) if (ref_ids[i] in testindex) else \
-                    val_index.append(i)
-        if status == 'train':
+            train_index.append(i) if (ref_ids[i] in trainindex) else test_index.append(
+                i
+            ) if (ref_ids[i] in testindex) else val_index.append(i)
+        if status == "train":
             self.index = train_index
             print("# Train Images: {}".format(len(self.index)))
-            print('Ref Index:')
+            print("Ref Index:")
             print(trainindex)
-        if status == 'test':
+        if status == "test":
             self.index = test_index
             print("# Test Images: {}".format(len(self.index)))
-            print('Ref Index:')
+            print("Ref Index:")
             print(testindex)
-        if status == 'val':
+        if status == "val":
             self.index = val_index
             print("# Val Images: {}".format(len(self.index)))
 
-        self.mos = Info['subjective_scores'][0, self.index]
-        self.mos_std = Info['subjective_scoresSTD'][0, self.index]
-        im_names = [Info[Info['im_names'][0, :][i]][()].tobytes()\
-                        [::2].decode() for i in self.index]
-        
+        self.mos = Info["subjective_scores"][0, self.index]
+        self.mos_std = Info["subjective_scoresSTD"][0, self.index]
+        im_names = [
+            Info[Info["im_names"][0, :][i]][()].tobytes()[::2].decode()
+            for i in self.index
+        ]
+
         self.patches = ()
         self.label = []
         self.label_std = []
@@ -83,13 +91,13 @@ class IQADataset(Dataset):
             im = self.loader(os.path.join(im_dir, im_names[idx]))
 
             patches = NonOverlappingCropPatches(im, self.patch_size, self.stride)
-            if status == 'train':
-                self.patches = self.patches + patches #
+            if status == "train":
+                self.patches = self.patches + patches  #
                 for i in range(len(patches)):
                     self.label.append(self.mos[idx])
                     self.label_std.append(self.mos_std[idx])
             else:
-                self.patches = self.patches + (torch.stack(patches), ) #
+                self.patches = self.patches + (torch.stack(patches),)  #
                 self.label.append(self.mos[idx])
                 self.label_std.append(self.mos_std[idx])
 
@@ -97,4 +105,7 @@ class IQADataset(Dataset):
         return len(self.patches)
 
     def __getitem__(self, idx):
-        return self.patches[idx], (torch.Tensor([self.label[idx]]), torch.Tensor([self.label_std[idx]]))
+        return (
+            self.patches[idx],
+            (torch.Tensor([self.label[idx]]), torch.Tensor([self.label_std[idx]])),
+        )
--- test_demo.py
+++ test_demo.py
@@ -3,7 +3,6 @@ Test Demo
     ```bash
     python test_demo.py --im_path=data/I03_01_1.bmp
     ```
- Date: 2018/5/26
 """
 
 from argparse import ArgumentParser
@@ -16,53 +15,69 @@ from IQADataset import NonOverlappingCropPatches
 
 class CNNIQAnet(nn.Module):
     def __init__(self, ker_size=7, n_kers=50, n1_nodes=800, n2_nodes=800):
+        """data: channel 1 => gray image patch"""
         super(CNNIQAnet, self).__init__()
-        self.conv1  = nn.Conv2d(1, n_kers, ker_size)
-        self.fc1    = nn.Linear(2 * n_kers, n1_nodes)
-        self.fc2    = nn.Linear(n1_nodes, n2_nodes)
-        self.fc3    = nn.Linear(n2_nodes, 1)
+        self.conv1 = nn.Conv2d(1, n_kers, ker_size)
+        self.fc1 = nn.Linear(2 * n_kers, n1_nodes)
+        self.fc2 = nn.Linear(n1_nodes, n2_nodes)
+        self.fc3 = nn.Linear(n2_nodes, 1)
 
     def forward(self, x):
-        x  = x.view(-1, x.size(-3), x.size(-2), x.size(-1))  #
+        # batch, channel, width, height -> 165,1,32,32
+        x = x.view(-1, x.size(-3), x.size(-2), x.size(-1))
 
-        h  = self.conv1(x)
+        # 32-3-3 -> 165,50,26,26
+        h = self.conv1(x)
 
-        # h1 = F.adaptive_max_pool2d(h, 1)
-        # h2 = -F.adaptive_max_pool2d(-h, 1)
+        # to 1 -> 165,50,1,1
         h1 = F.max_pool2d(h, (h.size(-2), h.size(-1)))
+        # min pool -> to 1 -> 165,50,1,1
         h2 = -F.max_pool2d(-h, (h.size(-2), h.size(-1)))
-        h  = torch.cat((h1, h2), 1)  # max-min pooling
-        h  = h.squeeze(3).squeeze(2)
 
-        h  = F.relu(self.fc1(h))
-        h  = F.dropout(h)
-        h  = F.relu(self.fc2(h))
+        # stack -> new dim, cat -> no new dim => 165,100,1,1
+        h = torch.cat((h1, h2), 1)  # max-min pooling
 
-        q  = self.fc3(h)
+        # remove 2,3 -> 165,100
+        h = h.squeeze(3).squeeze(2)
+
+        # 800 -> 165,800
+        h = F.relu(self.fc1(h))
+        h = F.dropout(h)
+
+        # 800 -> 165,800
+        h = F.relu(self.fc2(h))
+
+        # 1 -> 165,1
+        q = self.fc3(h)
         return q
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description='PyTorch CNNIQA test demo')
-    parser.add_argument("--im_path", type=str, default='data/I03_01_1.bmp',
-                        help="image path")
-    parser.add_argument("--model_file", type=str, default='models/CNNIQA-LIVE',
-                        help="model file (default: models/CNNIQA-LIVE)")
+    parser = ArgumentParser(description="PyTorch CNNIQA test demo")
+    parser.add_argument(
+        "--im_path", type=str, default="data/I03_01_1.bmp", help="image path"
+    )
+    parser.add_argument(
+        "--model_file",
+        type=str,
+        default="models/CNNIQA-LIVE",
+        help="model file (default: models/CNNIQA-LIVE)",
+    )
 
     args = parser.parse_args()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = CNNIQAnet(ker_size=7,
-                      n_kers=50,
-                      n1_nodes=800,
-                      n2_nodes=800).to(device)
+    model = CNNIQAnet(ker_size=7, n_kers=50, n1_nodes=800, n2_nodes=800).to(device)
 
-    model.load_state_dict(torch.load(args.model_file))
+    model.load_state_dict(torch.load(args.model_file, map_location=device))
 
-    im = Image.open(args.im_path).convert('L')
+    # rgb->gray
+    im = Image.open(args.im_path).convert("L")
+    # im->batch*32*32
     patches = NonOverlappingCropPatches(im, 32, 32)
 
     model.eval()
     with torch.no_grad():
         patch_scores = model(torch.stack(patches).to(device))
+        # patch scores mean -> image quality
         print(patch_scores.mean().item())

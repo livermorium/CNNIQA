6905b17f8b2c24e771bc12d5e29f04cd0e1d690e
chazzhuang <chazzhuang@tencent.com>
2020-03-29 17:24:44 +0800

    comment

--- IQADataset.py
+++ IQADataset.py
@@ -13,7 +13,7 @@ import h5py
 
 
 def default_loader(path):
-    return Image.open(path).convert("L")  #
+    return Image.open(path).convert("L")  # rgba -> 8bit gray level
 
 
 def LocalNormalization(patch, P=3, Q=3, C=1):
@@ -29,6 +29,13 @@ def LocalNormalization(patch, P=3, Q=3, C=1):
 
 
 def NonOverlappingCropPatches(im, patch_size=32, stride=32):
+    """
+    无重叠 取样
+
+    从左上角开始，32*32取样，步长32，舍弃不满足 32*32 的边缘
+
+    取样内部 特别的标准化
+    """
     w, h = im.size
     patches = ()
     # non-overlapping
@@ -43,6 +50,27 @@ def NonOverlappingCropPatches(im, patch_size=32, stride=32):
 
 
 class IQADataset(Dataset):
+    """
+    LIVE 为例
+
+    29张原图，779张损失图
+
+    按照 29张原图+trainratio+testratio，将 779张 失真图 均匀划分 得到 训练集、验证集、测试集，并返回目标图片的patches
+
+    随机方式 29张原图 随机全排列成1000种，按照输入exp_id选择一种
+
+    /im_names,(1, 779),object --损失图名称 ref
+    /index,(29, 1000),float64 --原是图id 随机
+    /ref_ids,(1, 779),float64 --失真图-》原始图 映射
+    /ref_names,(1, 29),object --原始图名称 ref
+    /subjective_scores,(1, 779),float64 --图片主观质量分
+    /subjective_scoresSTD,(1, 779),float64 --图片主观质量分标准差（与均值的差
+
+    图片本身信息
+    H. R. Sheikh, "Image Quality Assessment Using Natural Scene Statistics," Ph.D. dissertation,
+    University of Texas at Austin, May 2004.
+    """
+
     def __init__(self, conf, exp_id=0, status="train", loader=default_loader):
         self.loader = loader
         im_dir = conf["im_dir"]
@@ -79,6 +107,7 @@ class IQADataset(Dataset):
         self.mos = Info["subjective_scores"][0, self.index]
         self.mos_std = Info["subjective_scoresSTD"][0, self.index]
         im_names = [
+            # H5pyDeprecationWarning: dataset.value has been deprecated. Use dataset[()] instead.
             Info[Info["im_names"][0, :][i]][()].tobytes()[::2].decode()
             for i in self.index
         ]
@@ -90,7 +119,10 @@ class IQADataset(Dataset):
             # print("Preprocessing Image: {}".format(im_names[idx]))
             im = self.loader(os.path.join(im_dir, im_names[idx]))
 
-            patches = NonOverlappingCropPatches(im, self.patch_size, self.stride)
+            patches = NonOverlappingCropPatches(
+                im, self.patch_size, self.stride
+            )  # m * 32*32
+            # train 每张图片一组，val/test 所有图片一组
             if status == "train":
                 self.patches = self.patches + patches  #
                 for i in range(len(patches)):
@@ -105,6 +137,7 @@ class IQADataset(Dataset):
         return len(self.patches)
 
     def __getitem__(self, idx):
+        """提供随机访问能力"""
         return (
             self.patches[idx],
             (torch.Tensor([self.label[idx]]), torch.Tensor([self.label_std[idx]])),
--- main.py
+++ main.py
@@ -13,8 +13,7 @@ Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition. 2
     ```
 
  Implemented by Dingquan Li
- Email: dingquanli@pku.edu.cn
- Date: 2019/11/8
+ Modified: zhisays@outlook.com
 """
 
 from argparse import ArgumentParser
@@ -41,7 +40,7 @@ def ensure_dir(path):
 
 
 def loss_fn(y_pred, y):
-    return F.l1_loss(y_pred, y[0])
+    return F.l1_loss(y_pred, y[0])  # 相比l2 对离群点、异常值 不敏感
 
 
 class IQAPerformance(Metric):
@@ -50,10 +49,11 @@ class IQAPerformance(Metric):
 
     `update` must receive output of the form (y_pred, y).
     """
+
     def reset(self):
         self._y_pred = []
-        self._y      = []
-        self._y_std  = []
+        self._y = []
+        self._y_std = []
 
     def update(self, output):
         y_pred, y = output
@@ -63,7 +63,7 @@ class IQAPerformance(Metric):
         self._y_pred.append(torch.mean(y_pred).item())
 
     def compute(self):
-        sq = np.reshape(np.asarray(self._y), (-1,))
+        sq = np.reshape(np.asarray(self._y), (-1,))  # to 1 dim
         sq_std = np.reshape(np.asarray(self._y_std), (-1,))
         q = np.reshape(np.asarray(self._y_pred), (-1,))
 
@@ -72,6 +72,7 @@ class IQAPerformance(Metric):
         plcc = stats.pearsonr(sq, q)[0]
         rmse = np.sqrt(((sq - q) ** 2).mean())
         mae = np.abs((sq - q)).mean()
+        # pred-gt > 2 gt_std(图片主观质量分标准差（与均值的差) 的比例
         outlier_ratio = (np.abs(sq - q) > 2 * sq_std).mean()
 
         return srocc, krocc, plcc, rmse, mae, outlier_ratio
@@ -80,43 +81,41 @@ class IQAPerformance(Metric):
 class CNNIQAnet(nn.Module):
     def __init__(self, ker_size=7, n_kers=50, n1_nodes=800, n2_nodes=800):
         super(CNNIQAnet, self).__init__()
-        self.conv1  = nn.Conv2d(1, n_kers, ker_size)
-        self.fc1    = nn.Linear(2 * n_kers, n1_nodes)
-        self.fc2    = nn.Linear(n1_nodes, n2_nodes)
-        self.fc3    = nn.Linear(n2_nodes, 1)
+        self.conv1 = nn.Conv2d(1, n_kers, ker_size)
+        self.fc1 = nn.Linear(2 * n_kers, n1_nodes)
+        self.fc2 = nn.Linear(n1_nodes, n2_nodes)
+        self.fc3 = nn.Linear(n2_nodes, 1)
 
     def forward(self, x):
-        x  = x.view(-1, x.size(-3), x.size(-2), x.size(-1))  #
+        x = x.view(-1, x.size(-3), x.size(-2), x.size(-1))  #
 
-        h  = self.conv1(x)
+        h = self.conv1(x)
 
-        # h1 = F.adaptive_max_pool2d(h, 1)
-        # h2 = -F.adaptive_max_pool2d(-h, 1)
+        # max-min pooling
         h1 = F.max_pool2d(h, (h.size(-2), h.size(-1)))
         h2 = -F.max_pool2d(-h, (h.size(-2), h.size(-1)))
-        h  = torch.cat((h1, h2), 1)  # max-min pooling
-        h  = h.squeeze(3).squeeze(2)
+        h = torch.cat((h1, h2), 1)
+        h = h.squeeze(3).squeeze(2)  # 去维度
 
-        h  = F.relu(self.fc1(h))
-        h  = F.dropout(h)
-        h  = F.relu(self.fc2(h))
+        h = F.relu(self.fc1(h))
+        h = F.dropout(h)
+        h = F.relu(self.fc2(h))
 
-        q  = self.fc3(h)
+        q = self.fc3(h)
         return q
 
 
 def get_data_loaders(config, train_batch_size, exp_id=0):
-    train_dataset = IQADataset(config, exp_id, 'train')
-    train_loader = torch.utils.data.DataLoader(train_dataset,
-                                              batch_size=train_batch_size,
-                                              shuffle=True,
-                                              num_workers=4)
+    train_dataset = IQADataset(config, exp_id, "train")
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=train_batch_size, shuffle=True
+    )
 
-    val_dataset = IQADataset(config, exp_id, 'val')
+    val_dataset = IQADataset(config, exp_id, "val")
     val_loader = torch.utils.data.DataLoader(val_dataset)
 
-    if config['test_ratio']:
-        test_dataset = IQADataset(config, exp_id, 'test')
+    if config["test_ratio"]:
+        test_dataset = IQADataset(config, exp_id, "test")
         test_loader = torch.utils.data.DataLoader(test_dataset)
 
         return train_loader, val_loader, test_loader
@@ -124,17 +123,34 @@ def get_data_loaders(config, train_batch_size, exp_id=0):
     return train_loader, val_loader
 
 
-def run(train_batch_size, epochs, lr, weight_decay, config, exp_id, log_dir, trained_model_file, save_result_file, disable_gpu=False):
-    if config['test_ratio']:
-        train_loader, val_loader, test_loader = get_data_loaders(config, train_batch_size, exp_id)
+def run(
+    train_batch_size,
+    epochs,
+    lr,
+    weight_decay,
+    config,
+    exp_id,
+    log_dir,
+    trained_model_file,
+    save_result_file,
+    disable_gpu=False,
+):
+    if config["test_ratio"]:
+        train_loader, val_loader, test_loader = get_data_loaders(
+            config, train_batch_size, exp_id
+        )
     else:
         train_loader, val_loader = get_data_loaders(config, train_batch_size, exp_id)
 
-    device = torch.device("cuda" if not disable_gpu and torch.cuda.is_available() else "cpu")
-    model = CNNIQAnet(ker_size=config['kernel_size'],
-                      n_kers=config['n_kernels'],
-                      n1_nodes=config['n1_nodes'],
-                      n2_nodes=config['n2_nodes'])
+    device = torch.device(
+        "cuda" if not disable_gpu and torch.cuda.is_available() else "cpu"
+    )
+    model = CNNIQAnet(
+        ker_size=config["kernel_size"],
+        n_kers=config["n_kernels"],
+        n1_nodes=config["n1_nodes"],
+        n2_nodes=config["n2_nodes"],
+    )
     writer = SummaryWriter(log_dir=log_dir)
     model = model.to(device)
     print(model)
@@ -145,9 +161,9 @@ def run(train_batch_size, epochs, lr, weight_decay, config, exp_id, log_dir, tra
     global best_criterion
     best_criterion = -1  # SROCC>=-1
     trainer = create_supervised_trainer(model, optimizer, loss_fn, device=device)
-    evaluator = create_supervised_evaluator(model,
-                                            metrics={'IQA_performance': IQAPerformance()},
-                                            device=device)
+    evaluator = create_supervised_evaluator(
+        model, metrics={"IQA_performance": IQAPerformance()}, device=device
+    )
 
     @trainer.on(Events.ITERATION_COMPLETED)
     def log_training_loss(engine):
@@ -157,9 +173,12 @@ def run(train_batch_size, epochs, lr, weight_decay, config, exp_id, log_dir, tra
     def log_validation_results(engine):
         evaluator.run(val_loader)
         metrics = evaluator.state.metrics
-        SROCC, KROCC, PLCC, RMSE, MAE, OR = metrics['IQA_performance']
-        print("Validation Results - Epoch: {} SROCC: {:.4f} KROCC: {:.4f} PLCC: {:.4f} RMSE: {:.4f} MAE: {:.4f} OR: {:.2f}%"
-              .format(engine.state.epoch, SROCC, KROCC, PLCC, RMSE, MAE, 100 * OR))
+        SROCC, KROCC, PLCC, RMSE, MAE, OR = metrics["IQA_performance"]
+        print(
+            "Validation Results - Epoch: {} SROCC: {:.4f} KROCC: {:.4f} PLCC: {:.4f} RMSE: {:.4f} MAE: {:.4f} OR: {:.2f}%".format(
+                engine.state.epoch, SROCC, KROCC, PLCC, RMSE, MAE, 100 * OR
+            )
+        )
         writer.add_scalar("validation/SROCC", SROCC, engine.state.epoch)
         writer.add_scalar("validation/KROCC", KROCC, engine.state.epoch)
         writer.add_scalar("validation/PLCC", PLCC, engine.state.epoch)
@@ -173,15 +192,17 @@ def run(train_batch_size, epochs, lr, weight_decay, config, exp_id, log_dir, tra
             best_epoch = engine.state.epoch
             torch.save(model.state_dict(), trained_model_file)
 
-
     @trainer.on(Events.EPOCH_COMPLETED)
     def log_testing_results(engine):
-        if config["test_ratio"] > 0 and config['test_during_training']:
+        if config["test_ratio"] > 0 and config["test_during_training"]:
             evaluator.run(test_loader)
             metrics = evaluator.state.metrics
-            SROCC, KROCC, PLCC, RMSE, MAE, OR = metrics['IQA_performance']
-            print("Testing Results    - Epoch: {} SROCC: {:.4f} KROCC: {:.4f} PLCC: {:.4f} RMSE: {:.4f} MAE: {:.4f} OR: {:.2f}%"
-                  .format(engine.state.epoch, SROCC, KROCC, PLCC, RMSE, MAE, 100 * OR))
+            SROCC, KROCC, PLCC, RMSE, MAE, OR = metrics["IQA_performance"]
+            print(
+                "Testing Results    - Epoch: {} SROCC: {:.4f} KROCC: {:.4f} PLCC: {:.4f} RMSE: {:.4f} MAE: {:.4f} OR: {:.2f}%".format(
+                    engine.state.epoch, SROCC, KROCC, PLCC, RMSE, MAE, 100 * OR
+                )
+            )
             writer.add_scalar("testing/SROCC", SROCC, engine.state.epoch)
             writer.add_scalar("testing/KROCC", KROCC, engine.state.epoch)
             writer.add_scalar("testing/PLCC", PLCC, engine.state.epoch)
@@ -195,10 +216,13 @@ def run(train_batch_size, epochs, lr, weight_decay, config, exp_id, log_dir, tra
             model.load_state_dict(torch.load(trained_model_file))
             evaluator.run(test_loader)
             metrics = evaluator.state.metrics
-            SROCC, KROCC, PLCC, RMSE, MAE, OR = metrics['IQA_performance']
+            SROCC, KROCC, PLCC, RMSE, MAE, OR = metrics["IQA_performance"]
             global best_epoch
-            print("Final Test Results - Epoch: {} SROCC: {:.4f} KROCC: {:.4f} PLCC: {:.4f} RMSE: {:.4f} MAE: {:.4f} OR: {:.2f}%"
-                .format(best_epoch, SROCC, KROCC, PLCC, RMSE, MAE, 100 * OR))
+            print(
+                "Final Test Results - Epoch: {} SROCC: {:.4f} KROCC: {:.4f} PLCC: {:.4f} RMSE: {:.4f} MAE: {:.4f} OR: {:.2f}%".format(
+                    best_epoch, SROCC, KROCC, PLCC, RMSE, MAE, 100 * OR
+                )
+            )
             np.save(save_result_file, (SROCC, KROCC, PLCC, RMSE, MAE, OR))
 
     # kick everything off
@@ -208,38 +232,58 @@ def run(train_batch_size, epochs, lr, weight_decay, config, exp_id, log_dir, tra
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description='PyTorch CNNIQA')
+    parser = ArgumentParser(description="PyTorch CNNIQA")
     parser.add_argument("--seed", type=int, default=19920517)
-    parser.add_argument('--batch_size', type=int, default=128,
-                        help='input batch size for training (default: 128)')
-    parser.add_argument('--epochs', type=int, default=500,
-                        help='number of epochs to train (default: 500)')
-    parser.add_argument('--lr', type=float, default=0.001,
-                        help='learning rate (default: 0.001)')
-    parser.add_argument('--weight_decay', type=float, default=0.0,
-                        help='weight decay (default: 0.0)')
-    parser.add_argument('--config', default='config.yaml', type=str,
-                        help='config file path (default: config.yaml)')
-    parser.add_argument('--exp_id', default='0', type=str,
-                        help='exp id (default: 0)')
-    parser.add_argument('--database', default='LIVE', type=str,
-                        help='database name (default: LIVE)')
-    parser.add_argument('--model', default='CNNIQA', type=str,
-                        help='model name (default: CNNIQA)')
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=128,
+        help="input batch size for training (default: 128)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=500,
+        help="number of epochs to train (default: 500)",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=0.001, help="learning rate (default: 0.001)"
+    )
+    parser.add_argument(
+        "--weight_decay", type=float, default=0.0, help="weight decay (default: 0.0)"
+    )
+    parser.add_argument(
+        "--config",
+        default="config.yaml",
+        type=str,
+        help="config file path (default: config.yaml)",
+    )
+    parser.add_argument("--exp_id", default="0", type=str, help="exp id (default: 0)")
+    parser.add_argument(
+        "--database", default="LIVE", type=str, help="database name (default: LIVE)"
+    )
+    parser.add_argument(
+        "--model", default="CNNIQA", type=str, help="model name (default: CNNIQA)"
+    )
     # parser.add_argument('--resume', default=None, type=str,
     #                     help='path to latest checkpoint (default: None)')
-    parser.add_argument("--log_dir", type=str, default="logger",
-                        help="log directory for Tensorboard log output")
-    parser.add_argument('--disable_gpu', action='store_true',
-                        help='flag whether to disable GPU')
+    parser.add_argument(
+        "--log_dir",
+        type=str,
+        default="logger",
+        help="log directory for Tensorboard log output",
+    )
+    parser.add_argument(
+        "--disable_gpu", action="store_true", help="flag whether to disable GPU"
+    )
     # parser.add_argument('--multi_gpu', action='store_true',
     #                     help='flag whether to use multiple GPUs')
 
     args = parser.parse_args()
 
     torch.manual_seed(args.seed)  #
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True  # 确定性卷积，相当于把所有操作的seed=0，以便重现，会变慢
+    torch.backends.cudnn.benchmark = False  # 使用benchmark以启动CUDNN_FIND自动寻找最快的操作，当计算图不会改变的时候（每次输入形状相同，模型不改变）的情况下可以提高性能，反之则降低性能
     np.random.seed(args.seed)
     random.seed(args.seed)
 
@@ -247,18 +291,39 @@ if __name__ == "__main__":
 
     with open(args.config) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-    print('exp id: ' + args.exp_id)
-    print('database: ' + args.database)
-    print('model: ' + args.model)
+    print("exp id: " + args.exp_id)
+    print("database: " + args.database)
+    print("model: " + args.model)
+    # like dict update, config[args.database] attrs -> config attrs
     config.update(config[args.database])
     config.update(config[args.model])
 
-    log_dir = '{}/EXP{}-{}-{}-lr={}-{}'.format(args.log_dir, args.exp_id, args.database, args.model, args.lr, 
-                                                  datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"))
-    ensure_dir('checkpoints')
-    trained_model_file = 'checkpoints/{}-{}-EXP{}-lr={}'.format(args.model, args.database, args.exp_id, args.lr)
-    ensure_dir('results')
-    save_result_file = 'results/{}-{}-EXP{}-lr={}'.format(args.model, args.database, args.exp_id, args.lr)
+    log_dir = "{}/EXP{}-{}-{}-lr={}-{}".format(
+        args.log_dir,
+        args.exp_id,
+        args.database,
+        args.model,
+        args.lr,
+        datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+    )
+    ensure_dir("checkpoints")
+    trained_model_file = "checkpoints/{}-{}-EXP{}-lr={}".format(
+        args.model, args.database, args.exp_id, args.lr
+    )
+    ensure_dir("results")
+    save_result_file = "results/{}-{}-EXP{}-lr={}".format(
+        args.model, args.database, args.exp_id, args.lr
+    )
 
-    run(args.batch_size, args.epochs, args.lr, args.weight_decay, config, args.exp_id,
-        log_dir, trained_model_file, save_result_file, args.disable_gpu)
+    run(
+        args.batch_size,
+        args.epochs,
+        args.lr,
+        args.weight_decay,
+        config,
+        args.exp_id,
+        log_dir,
+        trained_model_file,
+        save_result_file,
+        args.disable_gpu,
+    )
